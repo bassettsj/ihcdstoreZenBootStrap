7db304ed59088b9f5420d9b75ba279c5383e0e84
bassettsj <steven.j.bassett@gmail.com>
2012-07-19 10:57:57 -0400

    Adding the base files from theme development so far

---
+++ js/script.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+var $ = jQuery;
+/*
+var $tilesDiv = $('.tiles > div');
+$tilesDiv.imagesLoaded( function(){
+	
+	$tilesDiv.masonry({
+  	itemSelector: '.tiles > div > div',
+  	fitWidth: true;
+  	isAnimated: true,
+  	animationOptions: {
+    	duration: 200
+    	},
+    gutterWidth: 0
+  });
+});
+*/
+/* change the colors of the tiles randomly */
+
+
+};
